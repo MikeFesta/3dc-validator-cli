8ca9af4dcd86335320c13b65b4802da2e26181c7
Mike Festa <mike@festa.tech>
2022-08-04 12:49:09 +0200

    alpha-6 with support for product info

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import chalk from 'chalk'; // For colored output
-import { Validator } from '@mikefesta/3dc-validator';
+import chalk from "chalk"; // For colored output
+import { Validator } from "@mikefesta/3dc-validator";
 
 // Extract the current working path
 const getPathFromCommandLineArgs = () => {
   const path = process.argv[1];
-  return path.substring(0, path.lastIndexOf('/') + 1);
+  return path.substring(0, path.lastIndexOf("/") + 1);
 };
 
 // Extract the full path of the provided schema (1st argument after index.js)
 const getSchemaPathFromCommandLineArgs = () => {
   if (process.argv.length < 3) {
-    throw new Error('A schema and 3D model need to be provided as arguments')
+    throw new Error("A schema and 3D model need to be provided as arguments");
   }
   const path = getPathFromCommandLineArgs();
   const schema = process.argv[2];
@@ -20,23 +20,27 @@ const getSchemaPathFromCommandLineArgs = () => {
 // Extract the full path of the provided 3d model (2nd argument after index.js)
 const getModelPathFromCommandLineArgs = () => {
   if (process.argv.length < 4) {
-    throw new Error('A 3D model needs to be provided as the second argument')
+    throw new Error("A 3D model needs to be provided as the second argument");
   }
   const path = getPathFromCommandLineArgs();
   const glb = process.argv[3];
   return path + glb;
 };
 
+// Extract the full path of the optional product information json (3rd argument after index.js)
+const getProductInfoPathFromCommandLineArgs = () => {
+  if (process.argv.length < 5) {
+    return "";
+  }
+  const path = getPathFromCommandLineArgs();
+  const productInfo = process.argv[4];
+  return path + productInfo;
+};
+
 // Print a message at the start of the program
 const printWelcomeMessage = (version) => {
-  console.log(
-    chalk.green(
-      '-- 3D COMMERCE VALIDATOR --'
-    )
-  );
-  console.log(
-    chalk.yellow('* Version: ' + version )
-  );
+  console.log(chalk.green("-- 3D COMMERCE VALIDATOR --"));
+  console.log(chalk.yellow("* Version: " + version));
 };
 
 // START
@@ -52,39 +56,45 @@ try {
   const glbFullPath = getModelPathFromCommandLineArgs();
   await validator.model.loadFromFileSystem(glbFullPath);
 
-  // 3. Run Validation
+  // 3. Load Product Info (optional)
+  const productInfoFullPath = getProductInfoPathFromCommandLineArgs();
+  if (productInfoFullPath) {
+    await validator.productInfo.loadFromFileSystem(productInfoFullPath);
+  }
+
+  // 4. Run Validation
   validator.generateReport();
 
   // Helpful to print the whole JSON object for testing during development
   //console.log(validator);
 
-  // 4. Show Report
+  // 5. Show Report
   // for formatting, find the length of the longest name
   let longestNameLength = 0;
-  validator.report.getItems().forEach(item => {
+  validator.report.getItems().forEach((item) => {
     if (item.name.length > longestNameLength) {
       longestNameLength = item.name.length;
     }
   });
-  console.log(chalk.magenta('==== Validation Report ===='));
+  console.log(chalk.magenta("==== Validation Report ===="));
   // Loop through all available items in the report and print their status
-  validator.report.getItems().forEach(item => {
-    let itemNameFormatted = item.name + ': ';
+  validator.report.getItems().forEach((item) => {
+    let itemNameFormatted = item.name + ": ";
     for (let i = item.name.length; i < longestNameLength; i++) {
-      itemNameFormatted = ' ' + itemNameFormatted;
+      itemNameFormatted = " " + itemNameFormatted;
     }
     console.log(
       itemNameFormatted +
-      (item.tested ?
-        (item.pass ? chalk.green('PASS') : chalk.red('FAIL'))
-        :
-        chalk.gray('NOT TESTED')
-      ) + ' | ' +
-      chalk.gray(item.message)
+        (item.tested
+          ? item.pass
+            ? chalk.green("PASS")
+            : chalk.red("FAIL")
+          : chalk.gray("NOT TESTED")) +
+        " | " +
+        chalk.gray(item.message)
     );
   });
-  console.log(chalk.magenta('==========================='));
+  console.log(chalk.magenta("==========================="));
 } catch (err) {
-  console.log(chalk.red('ERROR: ' + err.message));
+  console.log(chalk.red("ERROR: " + err.message));
 }
-
