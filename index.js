34d5f051a396ac3dfcdfec13d5b8daf117bc2688
Mike Festa <mike@festa.tech>
2022-12-16 10:24:55 -0500

    version 1.0.0-rc.8

--- index.js
+++ index.js
@@ -1,42 +1,144 @@
 import chalk from "chalk"; // For colored output
+import fs from "fs";
+import path from "path";
 import { Validator } from "@mikefesta/3dc-validator";
-import { exit } from "process";
+import { argv, exit } from "process";
 
-// Extract the current working path
-const getPathFromCommandLineArgs = () => {
-  const path = process.argv[1];
-  return path.substring(0, path.lastIndexOf("/") + 1);
-};
+let glbFilename = "";
+let gltfFileName = "";
+let gltfSupportingFiles = [];
+let i = 0;
+let jsonNoArgumentFileCount = 0;
+let jsonNoArgumentFilename = "";
+let outputIndex = -1;
+let outputCsvFilename = "";
+let outputJsonFilename = "";
+let productInfoIndex = -1;
+let productInfoFilename = "";
+let schemaIndex = -1;
+let schemaFilename = "";
 
-// Extract the full path of the provided schema (1st argument after index.js)
-const getSchemaPathFromCommandLineArgs = () => {
-  if (process.argv.length < 3) {
-    throw new Error("A schema and 3D model need to be provided as arguments");
+argv.forEach((arg) => {
+  // 0: node full path
+  // 1: index.js location
+  // optional arguments:
+  // -o output filename (ends with .json or .csv)
+  // -p product dimensions (ends with .json)
+  // required arguments:
+  // -s schema (ends with .json)
+  // .glb or .gltf + .bin (+ images)
+
+  // Supported file extensions
+  if (arg.endsWith(".csv")) {
+    outputCsvFilename = arg;
+  } else if (arg.endsWith(".glb")) {
+    glbFilename = arg;
+  } else if (arg.endsWith(".gltf")) {
+    gltfFileName = arg;
+  } else if (arg.endsWith(".json")) {
+    // .json can be for the schema, product info, or output
+    if (i == outputIndex) {
+      // -o
+      outputJsonFilename = arg;
+    } else if (i == productInfoIndex) {
+      // -p
+      productInfoFilename = arg;
+    } else if (i == schemaIndex) {
+      // -s
+      schemaFilename = arg;
+    } else {
+      // json file without an argument
+      jsonNoArgumentFilename = arg;
+      // If only one .json filename is provided without an argument, treat it as the schema
+      jsonNoArgumentFileCount++;
+    }
+  } else if (
+    [".bin", ".jpg", "jpeg", ".ktx", "ktx2", ".png", "webp"].includes(
+      arg.slice(-4)
+    )
+  ) {
+    // TODO: I believe these are the only file extensions supported, but user testing my reveal others
+    gltfSupportingFiles.push(arg);
   }
-  const path = getPathFromCommandLineArgs();
-  const schema = process.argv[2];
-  return path + schema;
-};
 
-// Extract the full path of the provided 3d model (2nd argument after index.js)
-const getModelPathFromCommandLineArgs = () => {
-  if (process.argv.length < 4) {
-    throw new Error("A 3D model needs to be provided as the second argument");
+  // Flags
+  if (arg == "-o") {
+    // output to a .csv or .json or both
+    // If no file name is provided, it defaults to {model_name} + timestamp.json
+    outputIndex = i + 1;
   }
-  const path = getPathFromCommandLineArgs();
-  const glb = process.argv[3];
-  return path + glb;
-};
+  if (arg == "-p") {
+    // specify product information (dimensions)
+    productInfoIndex = i + 1;
+  }
+  if (arg == "-s") {
+    // specify the schema (optional). A .json file without a preceding argument will be treated as the schema.
+    schemaIndex = i + 1;
+  }
 
-// Extract the full path of the optional product information json (3rd argument after index.js)
-const getProductInfoPathFromCommandLineArgs = () => {
-  if (process.argv.length < 5) {
-    return "";
+  i++;
+});
+
+// Verify that either a glb or gltf was provided
+if (glbFilename == "" && gltfFileName == "") {
+  console.log(chalk.red("A .glb or .gltf file needs to be provided"));
+  exit(1);
+}
+
+// If only one .json file name was provided without an argument, treat it as the schema
+if (jsonNoArgumentFileCount == 1) {
+  schemaFilename = jsonNoArgumentFilename;
+}
+
+// If just -o was provided without a filename ending in csv or json, generate a filename
+if (outputIndex > 0 && outputJsonFilename == "" && outputCsvFilename == "") {
+  const date = new Date();
+  const timestamp = [
+    date.getFullYear(),
+    ("0" + (date.getMonth() + 1)).slice(-2),
+    ("0" + date.getDate()).slice(-2),
+    ("0" + date.getHours()).slice(-2),
+    ("0" + date.getMinutes()).slice(-2),
+    ("0" + date.getSeconds()).slice(-2),
+  ].join("-");
+  if (glbFilename) {
+    outputJsonFilename =
+      glbFilename.substring(
+        glbFilename.lastIndexOf(path.sep) + 1,
+        glbFilename.length - 4
+      ) +
+      "-3DQC-" +
+      timestamp +
+      ".json";
+  } else if (gltfFileName) {
+    outputJsonFilename =
+      gltfFileName.substring(
+        gltfFileName.lastIndexOf(path.sep) + 1,
+        gltfFileName.length - 5
+      ) +
+      "-3DQC-" +
+      timestamp +
+      ".json";
   }
-  const path = getPathFromCommandLineArgs();
-  const productInfo = process.argv[4];
-  return path + productInfo;
-};
+}
+
+// Verify that a schema was provided
+if (schemaFilename == "") {
+  console.log(chalk.red("A schema file needs to be provided"));
+  exit(1);
+}
+
+if (
+  gltfFileName &&
+  !gltfSupportingFiles.map((i) => i.slice(-4)).includes(".bin")
+) {
+  console.log(
+    chalk.red(
+      "Using a .gltf file also needs to include a .bin file plus external textures"
+    )
+  );
+  exit(1);
+}
 
 // Print a message at the start of the program
 const printWelcomeMessage = (version) => {
@@ -52,17 +154,18 @@ try {
   printWelcomeMessage(validator.version);
 
   // 1. Load Schema (schema should be loaded before model to know if index calculation is required)
-  const schemaFullPath = getSchemaPathFromCommandLineArgs();
-  await validator.schema.loadFromFileSystem(schemaFullPath);
+  await validator.schema.loadFromFileSystem(schemaFilename);
 
-  // 2. Load GLB
-  const glbFullPath = getModelPathFromCommandLineArgs();
-  await validator.model.loadFromFileSystem(glbFullPath);
+  // 2. Load GLB or GLTF + files
+  // If a glb path exists, use it, otherwise use the gltf and supporting files
+  let fileList = glbFilename
+    ? [glbFilename]
+    : [gltfFileName, ...gltfSupportingFiles];
+  await validator.model.loadFromFileSystem(fileList);
 
   // 3. Load Product Info (optional)
-  const productInfoFullPath = getProductInfoPathFromCommandLineArgs();
-  if (productInfoFullPath) {
-    await validator.productInfo.loadFromFileSystem(productInfoFullPath);
+  if (productInfoFilename) {
+    await validator.productInfo.loadFromFileSystem(productInfoFilename);
   }
 
   // 4. Run Validation
@@ -105,6 +208,30 @@ try {
   console.log(
     "Total Time: " + ((Date.now() - startTime) / 1000).toFixed(3) + " seconds."
   );
+  if (outputCsvFilename) {
+    await fs.writeFileSync(
+      outputCsvFilename,
+      validator.getReportCsv(),
+      {},
+      (err) => {
+        if (err) {
+          console.log("there was an error writing the csv file");
+        }
+      }
+    );
+  }
+  if (outputJsonFilename) {
+    await fs.writeFileSync(
+      outputJsonFilename,
+      validator.getReportJson(),
+      {},
+      (err) => {
+        if (err) {
+          console.log("there was an error writing the csv file");
+        }
+      }
+    );
+  }
 } catch (err) {
   if (err) {
     console.log(chalk.red("ERROR: " + err.message));
